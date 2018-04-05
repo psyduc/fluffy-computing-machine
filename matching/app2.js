c5ed123c9a376076f7deb1cda0d79c97be506d29
Duc Ngo <duc.ngos.it.all@gmail.com>
2018-04-05 12:24:19 -0700

    Matching #1 Fixed a bug where you can click on the same tile after already clicking it. Also fixed the setTimeout function to delay the flipback function after one second.

--- matching/app2.js
+++ matching/app2.js
@@ -22,6 +22,7 @@ function newBoard() {
 	for(var i = 0; i < matching_array.length; i++) {
 		var currentCard = document.getElementById('card' + i);
 		console.log(currentCard);
+		currentCard.style.background === '';
 		currentCard.classList.add(matching_array[i]);
 	}
 }
@@ -43,8 +44,9 @@ function newBoard() {
 			//if cardlipped length === colorarray length
 				//winner	
 function flipCard(card) {
-	var color = card.className;
-	if (card.style.background !== color){
+	var color = card.className.split(' ')[0];
+	if (card.style.background === color){
+	} else {
 		if(colorArray.length === 0) {
 		card.classList.add('active');
 		card.style.background = color;
@@ -54,9 +56,10 @@ function flipCard(card) {
 			card.classList.add('active');
 			card.style.background = color;
 			colorArray.push(color);
+			console.log(colorArray);
 			if(colorArray[0] != colorArray[1]) {
-				setTimeout(flipback(),1000);
-				removeActive();
+				setTimeout("flipback()",1000);
+				setTimeout("removeActive()",1000);
 				colorArray = [];
 			} else /*the colors in the array are they same*/{
 				//remove active class
@@ -65,9 +68,9 @@ function flipCard(card) {
 				cardsFlipped += 2;
 			}
 		}
-		if (cardsFlipped === matching_array.length) {
-			alert('Congrats you finished');
-		}
+	}
+	if (cardsFlipped === matching_array.length) {
+		alert('Congrats you finished');
 	}
 }
 
